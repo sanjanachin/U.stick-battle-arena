f788450f387188bfd5943a38f4bc462e81c046dc
JimmyCheong <c7834@uw.edu>
2023-01-29 17:53:18 -0800

    Added GameplayManager Added GameplayService script and SO

---
+++ Assets/Scripts/GameplayService.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Game
+{
+    /**
+     * Provides references and common functions to any gameObjects
+     * Should ONLY be referenced by gameObjects in Gameplay scenes
+     */
+    // Commented for the singleton property of the class
+    // [CreateAssetMenu(menuName = "Game/GameplayService", fileName = "GameplayService")]
+    public class GameplayService : ScriptableObject
+    {
+        public ProjectileManager ProjectileManager { get; private set; }
+
+        /**
+         * Any provide function below are to setup the reference of
+         * managers of the gameplay scene.
+         * Should ONLY be called by the GameplayManager of the scene.
+         */
+        public void ProvideProjectileManager(ProjectileManager projectileManager)
+        {
+            ProjectileManager = projectileManager;
+        }
+    }
+}
