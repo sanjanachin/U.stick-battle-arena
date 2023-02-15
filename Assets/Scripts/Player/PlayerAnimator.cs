890de8b8cbf9ca8538cb633a3beb6d7baed9d555
BuildTools <unconfigured@null.spigotmc.org>
2023-02-14 16:29:49 -0800

    Actually cleaned up the comments on the animation controller

--- Assets/Scripts/Player/PlayerAnimator.cs
+++ Assets/Scripts/Player/PlayerAnimator.cs
@@ -5,21 +5,15 @@ using Random = UnityEngine.Random;
 
 namespace Game.Player {
     public class PlayerAnimator : MonoBehaviour {
-        // [SerializeField] private float _minImpactForce = 20;
-        
-        // Anim times can be gathered from the state itself, but 
-        // for the simplicity of the video...
-        // [SerializeField] private float _landAnimDuration = 0.1f;
-        // [SerializeField] private float _attackAnimTime = 0.2f;
 
         // maxTilt and tiltSpeed control rate and maximum pitch of rotation effect while moving
         [SerializeField] private float _maxTilt = 4;
         [SerializeField] private float _tiltSpeed = 20;
-        [SerializeField] private float _landAnimDuration = .1f;
+        // landAnimDuration controls the length of the landing animation
+        [SerializeField] private float _landAnimDuration = .45f;
 
         private IPlayerController _player;
         private SpriteRenderer _renderer;
-        
         private Animator _anim;
         private float _lockedTill;
 
@@ -30,8 +24,8 @@ namespace Game.Player {
                 Destroy(this);
                 return;
             }
-
             _player = player;
+            // Grab animations from animator
             _anim = GetComponent<Animator>();
             _renderer = GetComponent<SpriteRenderer>();
         }
@@ -42,8 +36,8 @@ namespace Game.Player {
             var targetRotVector = new Vector3(0, 0, Mathf.Lerp(-_maxTilt, _maxTilt, Mathf.InverseLerp(-1, 1, _player.MovementInput.X)));
             _anim.transform.rotation = Quaternion.RotateTowards(_anim.transform.rotation, Quaternion.Euler(targetRotVector), _tiltSpeed * Time.deltaTime);
             var state = GetState();
-
             if (state == _currentState) return;
+            // Apply animation changes
             _anim.CrossFade(state, 0, 0);
             _currentState = state;
         }
@@ -53,26 +47,17 @@ namespace Game.Player {
             if (Time.time < _lockedTill) return _currentState;
 
             // Animations sorted by priority
-            // if (_player.JumpingThisFrame) return Jump;
             if (_player.LandingThisFrame) return LockState(Land, _landAnimDuration);
             if (!_player.Grounded) return Jump;
             if (_player.MovementInput.X == 0) return Idle;
             if (_player.MovementInput.X != 0) return Walk;
-
-            // if (_player.Grounded) return _player.MovementInput.X == 0 ? Idle : Walk;
+            
+            // Lock the animation for t amount of time, used when you do not want to be interrupted
             int LockState(int s, float t) {
                 _lockedTill = Time.time + t;
                 return s;
             }
             return Idle;
-
-            // Priorities
-            // if (_attacked) return LockState(Attack, _attackAnimTime);
-            // if (_player.Crouching) return Crouch;
-            // if (_landed) return LockState(Land, _landAnimDuration);
-
-            // return _player.Speed.y > 0 ? Jump : Fall;
-
         }
 
         #region Cached Properties
@@ -83,20 +68,7 @@ namespace Game.Player {
         private static readonly int Walk = Animator.StringToHash("Grounded");
         private static readonly int Jump = Animator.StringToHash("Jump");
         private static readonly int Land = Animator.StringToHash("Land");
-        // private static readonly int Fall = Animator.StringToHash("Fall");
-        // private static readonly int Attack = Animator.StringToHash("Attack");
-        // private static readonly int Crouch = Animator.StringToHash("Crouch");
 
         #endregion
     }
-
-    // public interface IPlayerController {
-    //     public Vector2 Input { get; }
-    //     public Vector2 Speed { get; }
-    //     public bool Crouching { get; }
-
-    //     public event Action<bool, float> GroundedChanged; // Grounded - Impact force
-    //     public event Action Jumped;
-    //     public event Action Attacked;
-    // }
 }
