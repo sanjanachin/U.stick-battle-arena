5ae033fe8e11fc7cdb17b92acd1cbbb67355cafa
JimmyCheong <c7834@uw.edu>
2023-02-26 23:20:43 -0800

    Disable used packages cleaned example test script to reduce testing lib included in build

--- Assets/Tests/Editor/example.cs
+++ Assets/Tests/Editor/example.cs
@@ -1,7 +1,7 @@
+#if (UNITY_EDITOR) 
+
 using System.Collections;
-using System.Collections.Generic;
 using NUnit.Framework;
-using UnityEngine;
 using UnityEngine.TestTools;
 
 public class example
@@ -24,3 +24,5 @@ public class example
         yield return null;
     }
 }
+
+#endif
